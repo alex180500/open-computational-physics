dfeb1075f01ee16587a3f9ff0961eb115185e894
alex180500 <30732244+alex180500@users.noreply.github.com>
2021-12-11 23:06:24 +0100

    added the whole heat-equation section

---
+++ heat-equation/README.md
@@ -0,0 +1,68 @@
+# Explicit 2D, an animated Heat equation PDE solver
+
+<img src="images/default.png" height="250"/> <img src="images/none.png" height="250"/> <img src="images/north-south.png" height="250"/>
+
+Explicit 2D is a *Python* script which calculate the solution of the [Heat PDE](https://en.wikipedia.org/wiki/Heat_equation) using an explicit [**FTCS**](https://en.wikipedia.org/wiki/FTCS_scheme) scheme. You can manually set the boundary conditions!
+
+## How do I change material or set boundary conditions?
+
+In order to change material just change the `-a` flag which sets the thermal diffusivity, it must be specified in **m^2/s**.
+
+By default the code uses Dirichelet (fixed temperature indicated by the `-Tb` flag) conditions on the top and right (N and E) and Neumann (zero gradient) conditions on bottom and left (S and W). If you want to change this you need to specify where to put the Neumann conditions, if you instead want only Dirichelet conditions everywhere (like in the middle figure), you specify the `--none` flag.
+
+Let's say I want Neumann conditions on N, S and W, you would run
+```console
+$ python explicit_2d.py nsw
+```
+The order of the letters is not important.
+
+## Examples
+
+Let's say you want to set the diffusivity to 1.27e-4 m^2/s and want to heat the top and the bottom (so top and bottom have Dirichelet conditions and **right** and **left** have Neumann conditions)
+```console
+$ python explicit_2d.py we -a 1.27e-4 
+```
+
+For all the other flags and options check out the help page or the code itself (which is thoroughly commented)
+```console
+$ python explicit_2d.py -h
+```
+
+## References
+
+The argument parsing via command line is done using the **argparse** module, please refer to the [official documentation](https://docs.python.org/3/library/argparse.html) and this great [Stack Overflow answer](https://stackoverflow.com/questions/20063/whats-the-best-way-to-parse-command-line-arguments).
+
+The implementation is based upon [**Numerical MOOC**](https://github.com/numerical-mooc/numerical-mooc) (*Practical Numerical Methods with Python*) by *Lorena A. Barba*, in particular the 4th lecture about [diffusion problem](https://nbviewer.org/github/numerical-mooc/numerical-mooc/blob/master/lessons/04_spreadout/04_03_Heat_Equation_2D_Explicit.ipynb).
+
+## Table of diffusivity
+
+This table is a cut version of the one from [**Wikipedia**](https://en.wikipedia.org/wiki/Thermal_diffusivity), all values are in **mm^2/s** needs to be multiplied by **1e-6** before being used in the code (so they get converted to m^2/s).
+
+
+| Material                 | Thermal Diffusivity |
+|--------------------------|---------------------|
+| Gold                     | 127                 |
+| Copper at 25 °C          | 111                 |
+| Aluminium                | 97                  |
+| Silicon                  | 88                  |
+| Tin                      | 40                  |
+| Iron                     | 23                  |
+| Air (300 K)              | 19                  |
+| Quartz                   | 1.4                 |
+| Sandstone                | 1.15                |
+| Ice at 0 °C              | 1.02                |
+| Brick, common            | 0.52                |
+| Glass, window            | 0.34                |
+| Brick, adobe             | 0.27                |
+| Water at 25 °C           | 0.143               |
+| Nylon                    | 0.09                |
+| Wood (yellow pine)       | 0.082               |
+| PVC (polyvinyl chloride) | 0.08                |
+| Alcohol                  | 0.07                |
+
+## Dependencies
+
+  * **python 3**
+  * **matplotlib**
+  * **numpy**
+  * **argparse** (which comes with your Python installation)
---
+++ heat-equation/explicit_2d.py
@@ -0,0 +1,152 @@
+# Explicit Euler method for heat equation pde with a cool animation
+#
+# Author: Alessandro Romancino
+# https://github.com/alex180500/open-computational-physics
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.animation import FuncAnimation
+import argparse
+
+# Argument parsing using the argparse module,
+# you can call help by using the '-h' or '--help' flag.
+parser = argparse.ArgumentParser(
+    description='2D Heat equation PDE solver using Euler method',
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('position', type=str, nargs='?',
+                    default='ne', help='''Select the position of Neumann conditions
+                    in the lattice (zero-gradient) can be "n", "s", "e", "w"
+                    or any combination like "ns" or "swe"''')
+parser.add_argument('--none', action='store_true',
+                    help='Use this arguments to remove all Neumann conditions')
+parser.add_argument('-a', type=float, default=1e-4,
+                    help='Thermal diffusivity in m^2/s')
+parser.add_argument('-nt', type=int, default=500,
+                    help='Number of time step to compute')
+parser.add_argument('-Tb', type=float, default=100.0,
+                    help='Default bath temperature in C')
+parser.add_argument('-T0', type=float, default=20.0,
+                    help='Default grid temperature in C')
+parser.add_argument('-Lx', type=float, default=0.01,
+                    help='Grid length of the x axis in meters')
+parser.add_argument('-Ly', type=float, default=0.01,
+                    help='Grid length of the y axis in meters')
+parser.add_argument('-nx', type=int, default=41,
+                    help='Number of grid points in the x axis')
+parser.add_argument('-ny', type=int, default=41,
+                    help='Number of grid points in the y axis')
+# arguments passed to the args class
+args = parser.parse_args()
+
+
+def ftcs(T0, nt, sigma_x, sigma_y):
+    """
+    Computes the temperature field with an explicit algorithm for nt
+    time steps. The algorithm uses a FTCS scheme with Dirichelet boundary 
+    conditions by default, with user-inputtable Neumann (zero-gradient)
+    boundary conditions at any side (N, S, W, E).
+
+    Parameters
+    ----------
+    T0 : ndarray
+        2D array containing the initial temperature field as float data.
+    nt : int
+        number of time steps to integrate.
+    sigma_x : float
+        constant present in the FTCS method for the x axis (alpha*dt/dx^2)
+    sigma_y : float
+        constant present in the FTCS method for the y axis (alpha*dt/dy^2)
+
+    Returns
+    -------
+    ndarray
+        3D array containing the 2D field at every step.
+    """
+    # 2d array for every step
+    T = T0.copy()
+    # shape of the 3d array, first axis represent all the time steps
+    shape = (nt,) + T.shape
+    # set the final 3d array
+    T_evol = np.zeros(shape)
+    # cycle for every time step
+    for n in range(nt):
+        # FTCS algorithm with default Dirichelet conditions
+        T[1:-1, 1:-1] = (T[1:-1, 1:-1] +
+                         sigma_x * (T[1:-1, 2:] - 2.0 * T[1:-1, 1:-1] + T[1:-1, :-2]) +
+                         sigma_y * (T[2:, 1:-1] - 2.0 * T[1:-1, 1:-1] + T[:-2, 1:-1]))
+        # check user input for Neumann conditions
+        if 'n' in args.position:
+            T[-1, :] = T[-2, :]
+        if 'e' in args.position:
+            T[:, -1] = T[:, -2]
+        if 's' in args.position:
+            T[0, :] = T[1, :]
+        if 'w' in args.position:
+            T[:, 0] = T[:, 1]
+        # assign the calculated field to the nth time step
+        T_evol[n] = T
+    return T_evol
+
+
+# grid spacing in the x direction
+dx = args.Lx / (args.nx - 1)
+# grid spacing in the y direction
+dy = args.Ly / (args.ny - 1)
+# define the grid positions
+x = np.linspace(0.0, args.Lx, args.nx)
+y = np.linspace(0.0, args.Ly, args.ny)
+# initial uniform temperature distribution
+T0 = np.full((args.ny, args.nx), args.T0)
+# reset Neumann conditions if the "--none" argument is used
+if args.none:
+    args.position = ''
+# set boundaries at fixed temperature if not specified
+if 'n' not in args.position:
+    T0[-1, :] = args.Tb
+if 'e' not in args.position:
+    T0[:, -1] = args.Tb
+if 's' not in args.position:
+    T0[0, :] = args.Tb
+if 'w' not in args.position:
+    T0[:, 0] = args.Tb
+
+# CFL limit is 0.25
+sigma = 0.25
+# set the minimum for the time step
+dt = sigma * min(dx, dy)**2 / args.a
+# compute the constant in FTCS
+sigma_x = args.a * dt / dx**2
+sigma_y = args.a * dt / dy**2
+# get the 3d array
+T = ftcs(T0, args.nt, sigma_x, sigma_y)
+
+# plotting with animation
+fig = plt.figure(figsize=(8.0, 5.0))
+# first contour plot
+contf = plt.contourf(x, y, T[0], levels=50, cmap=cm.plasma)
+# set the ticks in the colorbar (fixed)
+ticks = np.linspace(T[0].min(), T[0].max(), 5, endpoint=True)
+# plot the fixed colorbar
+plt.colorbar(contf, ticks=ticks, format='%.1f', label='Temperature [C]')
+# rescale the contour plot to be sized according to Lx and Ly
+plt.axis('scaled')
+
+
+def update(frame):
+    # set the contour plot to global
+    global contf
+    # remove all collections of the contourplot for better speed
+    for coll in contf.collections:
+        coll.remove()
+    # set the contourplot for the frame
+    contf = plt.contourf(x, y, T[frame], levels=50, cmap=cm.plasma)
+    return contf.collections
+
+
+# hard coded fps
+fps = 24
+# animation
+animation = FuncAnimation(fig, update, interval=1000/fps, frames=args.nt)
+# animation.save('images/animation.mp4', fps=fps)
+plt.show()
